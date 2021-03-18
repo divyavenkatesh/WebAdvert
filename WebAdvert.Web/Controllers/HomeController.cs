91ec8d8126958e000a19dc3a07bc5498e088682b
Divya Veeramani <divya.veeramani@credibleinc.com>
2021-03-18 20:36:42 +0530

    Initial Commit

---
+++ WebAdvert.Web/Controllers/HomeController.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using WebAdvert.Web.Models;
+using WebAdvert.Web.Models.Home;
+using WebAdvert.Web.ServiceClients;
+using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+
+namespace AdvertApi.Management.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ISearchApiClient SearchApiClient { get; }
+        public IMapper Mapper { get; }
+        public IAdvertApiClient ApiClient { get; }
+
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ISearchApiClient searchApiClient, IMapper mapper, IAdvertApiClient apiClient, ILogger<HomeController> logger)
+        {
+            _logger = logger;
+            SearchApiClient = searchApiClient;
+            Mapper = mapper;
+            ApiClient = apiClient;
+        }
+        
+        [Authorize]
+        [ResponseCache(Duration = 60)]
+        public async Task<IActionResult> Index()
+        {
+            var allAds = await ApiClient.GetAllAsync();
+            var allViewModels = allAds.Select(x => Mapper.Map<IndexViewModel>(x));
+
+            return View(allViewModels);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel {RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier});
+        }
+        
+        [HttpPost]
+        public async Task<IActionResult> Search(string keyword)
+        {
+            var viewModel = new List<SearchViewModel>();
+
+            var searchResult = await SearchApiClient.Search(keyword).ConfigureAwait(false);
+            searchResult.ForEach(advertDoc =>
+            {
+                var viewModelItem = Mapper.Map<SearchViewModel>(advertDoc);
+                viewModel.Add(viewModelItem);
+            });
+
+            return View("Search", viewModel);
+        }
+    }
+}
---
+++ WebAdvert.Web/Startup.cs
@@ -0,0 +1,108 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using WebAdvert.Web.ServiceClients;
+using WebAdvert.Web.Services;
+using Amazon.DynamoDBv2;
+using AutoMapper;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Polly;
+using Polly.Extensions.Http;
+
+namespace AdvertApi.Management.Web
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+            services.AddCognitoIdentity();
+
+            services.AddMvc();
+
+            services.ConfigureApplicationCookie(options =>
+            {
+                options.ExpireTimeSpan = TimeSpan.FromMinutes(5);
+                options.LoginPath = "/Accounts/Login";
+                options.SlidingExpiration = true;
+            });
+
+            services.AddAutoMapper(typeof(Startup));
+            services.AddTransient<IFileUploader, S3FileUploader>();
+
+            services.AddHttpClient<IAdvertApiClient, AdvertApiClient>()
+                .AddPolicyHandler(GetRetryPolicy())
+               .AddPolicyHandler(GetCircuitBreakerPolicy());
+            services.AddHttpClient<ISearchApiClient, SearchApiClient>().AddPolicyHandler(GetRetryPolicy())
+                .AddPolicyHandler(GetCircuitBreakerPolicy());
+
+            services.AddDefaultAWSOptions(Configuration.GetAWSOptions());
+            services.AddAWSService<IAmazonDynamoDB>();
+
+            services.AddControllersWithViews();
+        }
+
+        private IAsyncPolicy<HttpResponseMessage> GetCircuitBreakerPolicy()
+        {
+            return HttpPolicyExtensions.HandleTransientHttpError().CircuitBreakerAsync(3, TimeSpan.FromSeconds(30));
+        }
+
+        private IAsyncPolicy<HttpResponseMessage> GetRetryPolicy()
+        {
+            return HttpPolicyExtensions.HandleTransientHttpError()
+                .OrResult(msg => msg.StatusCode == HttpStatusCode.NotFound)
+                .WaitAndRetryAsync(5, retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)));
+        }
+
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+
+
+
+            app.UseStaticFiles();
+            app.UseRouting();
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
